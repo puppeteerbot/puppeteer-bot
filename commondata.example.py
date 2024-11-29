717e0170f54071dfa0a992370a40d0269b79492a
JuIcEPoopooumgood <josifbell@gmail.com>
2024-11-29 09:31:00 -0500

    Update commondata.example.py

--- commondata.example.py
+++ commondata.example.py
@@ -36,6 +36,7 @@ KirkaBadges = {
 }
 KirkaBackgrounds = {
     "Skynet": "https://i.imgur.com/NxItcCc.png",
+    "Flow": "https://juicepoopooumgood.github.io/Api-guy/flowbg.webp",
     "Glitchedbg": "https://i.imgur.com/IZ2tC5C.jpeg",
     # "TTV": "https://i.imgur.com/MJ13qJi.png"
     "TTV": "https://cdn.discordapp.com/avatars/1169111190824308768/5019045e780fd069aa2f36bddd72847c.png?size=1024",
